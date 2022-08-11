dd2e44eeb8a44b2f90641acc66d37681129eee8d
vrindaparam <vrindaangel@gmail.com>
2022-08-11 06:46:02 -0400

    program to check software installed

--- req_soft_check.py
+++ req_soft_check.py
@@ -8,6 +8,7 @@ def soft_check(filepath):
         print("Software is not installed")
 
 
-filepath = input("Enter the filepath of the software")
+filepath = input("Enter the filepath of the software: ")
 #filepath = "/usr/bin/customapp"
 soft_check(filepath)
+quit()
